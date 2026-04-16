1bd5ce0b3ab1c870b3687b4cd8c126546914fbf8
THEO GLINSKY <50237129@ort>
2026-04-16 08:34:32 -0300

    inicio de proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tp_final_bimestre_1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

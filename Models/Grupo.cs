a8da6c24f05a52e3eaa70162f035e87a1d959e6c
THEO GLINSKY <50237129@ort>
2026-04-21 08:19:28 -0300

    en proceso la clase grupo

--- Models/Grupo.cs
+++ Models/Grupo.cs
@@ -5,11 +5,33 @@ namespace tp_final_bimestre_1.Models
     public class Grupo
     {
         private Dictionary<int, Integrante> Integrantes;
-        //Integrante miIntegrante = new Integrante(50237129, theo, new DateTime(2010,2,3), perro, );
-        //Integrantes.Add(50237129, miIntegrante)
-        public Grupo
+
+        public void CargaDatosManual()
         {
-            Integrantes = new Dictionary();
+            List<string> intereses1 = new List<string>();
+            intereses1.Add("jugar al futbol");
+            intereses1.Add("mirar series");
+            intereses1.Add("tomar mate");
+            List<string> familiares1 = new List<string>();
+            familiares1.Add("ezequiel");
+            familiares1.Add("solange");
+            familiares1.Add("emma");
+            Integrante miIntegrante1 = new Integrante(50237129, theo, new DateTime(2010,2,3), perro, intereses1, familiares1);
+            Integrantes.Add(50237129, miIntegrante1);
+            List<string> intereses2 = new List<string>();
+            intereses2.Add("jugar basquet");
+            intereses2.Add("ir al gimnasio");
+            intereses2.Add("tocar justin en la guitarra");
+            List<string> familiares2 = new List<string>();
+            familiares2.Add("andrea");
+            familiares2.Add("ariel");
+            familiares2.Add("ema");
+            Integrante miIntegrante2 = new Integrante(50237157, dani, new DateTime(2010,12,5), gato, intereses2, familiares2);
+            Integrantes.Add(50237157, miIntegrante2);
+        }
+        public Dic<int, Integrante> DevolverIntegrantes()
+        {
+            return Integrantes;
         }
     }
 }
